252abe3be690a2f10d1f67758030c04cd99bd204
milagrosariveromunoz <milagrosarivero@icloud.com>
2023-01-11 20:23:32 +0000

    Entrega de archivo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let icon =['<i class="bi bi-arrows-fullscreen"></i>', 
+             '<i class="bi bi-at"></i>',
+             '<i class="bi bi-arrow-up-left-square"></i>',
+             '<i class="bi bi-circle-half"></i>'
+            ];
+              
+  
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class","right on time","when I finished","during my lunch","while I was praying"];
+  
+  let iconRandom = Math.floor(Math.random() * icon.length);
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  let excuse= icon[iconRandom]+ ' ' +who[whoRandom]+' '+what[whatRandom]+' '+when[whenRandom]+' ' +icon[iconRandom]
+  let element=document.querySelector('#excuse');
+  element.innerHTML=excuse;
+
+  console.log("excuse: ", excuse);
+  console.log("element: ", element);
+  console.log("innerHTML: ", element.innerHTML);
 };
